f55ef16f9ed4561a9352986302b89809f70a893b
Gaurav pathak <85889185+Gauravpathak10@users.noreply.github.com>
2022-06-28 21:11:22 +0530

    Update BookStoreRoute.js

--- server/Routes/BookStoreRoute.js
+++ server/Routes/BookStoreRoute.js
@@ -6,7 +6,7 @@ BookStore.post('/Add', CreateBook)
 BookStore.get('/get', FindAllBooks)
 BookStore.get('/get/:id', SpecificBookDetails)
 BookStore.put('/edit/:id', EditBook)
-BookStore.put('/discount/:id/:price', DiscountOffer)
+BookStore.patch('/discount/:id/:price', DiscountOffer)
 BookStore.delete('/delete/:id', DeleteBook)
 
 export default BookStore;
